663e9829d9c5aacd5dc686f67edf96eef33df231
Heron Yang <heron.yang.tw@gmail.com>
2023-09-24 11:01:32 -0700

    Add 2023-09-24 and pair on duty.

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ import random
 import sys
 from datetime import date
 
+ENABLE_PAIR_ON_DUTY = True
+
+def extract_name(line):
+  chunks = line.split(".")
+  return chunks[1].strip(), chunks[0].strip()
+
+def get_printed_name(name):
+  return name[0] + " (" + name[1] + ")"
+
 def main():
   filename = sys.argv[1]
   print("Filename (use for random seed as well): " + filename)
@@ -9,11 +18,12 @@ def main():
   with open(filename) as f:
     raw = f.read()
 
-  names = [(line.split(".")[1].strip(), line.split(".")[0].strip()) for line in raw.split("\n")[:-1]]
+  names = [extract_name(line) for line in raw.split("\n")[:-1]]
   slots = ["1:30", "2:00", "2:30", "3:00", "3:30", "4:00", "4:30", "5:00"]
 
   random.seed(filename)
-  selected = random.sample(names, len(slots))
+  selected_num_multipier = 2 if ENABLE_PAIR_ON_DUTY else 1
+  selected = random.sample(names, len(slots) * selected_num_multipier)
 
   print('Names:')
   for name in names:
@@ -21,7 +31,12 @@ def main():
 
   for i in range(len(slots)):
     end_slot = slots[i+1] if i + 1 < len(slots) else "end"
-    print(slots[i] + "-" + end_slot+": " + selected[i][0] + " (" + selected[i][1] + ")")
+    if ENABLE_PAIR_ON_DUTY:
+      on_duty_name = get_printed_name(selected[i*2]) + " + " \
+          + get_printed_name(selected[i*2 + 1])
+    else:
+      on_duty_name = get_printed_name(selected[i])
+    print(slots[i] + "-" + end_slot + ": " + on_duty_name)
 
 if __name__ == "__main__":
   main()

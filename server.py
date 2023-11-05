712ab048da0b66289f86b92ba75fc3b120f4314f
Heron Yang <heron.yang.tw@gmail.com>
2023-11-05 09:11:51 -0800

    Add server echo code.

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-  return "Hello World!"
+@app.route('/', methods=('GET', 'POST'))
+def homepage():
+  if request.method == 'POST':
+    content = request.form['content']
+    return render_template('homepage.html', result=content)
+  return render_template('homepage.html')

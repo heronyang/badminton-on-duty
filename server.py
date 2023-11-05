6b9a218734bdeab14672b7a0c7f905a4151f4178
Heron Yang <heron.yang.tw@gmail.com>
2023-11-05 10:35:28 -0800

    Add page with funcitons.

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
+from datetime import date
 from flask import Flask, render_template, request
+from shuffler import get_shuffle_response
 
 app = Flask(__name__)
 
@@ -6,5 +8,7 @@ app = Flask(__name__)
 def homepage():
   if request.method == 'POST':
     content = request.form['content']
-    return render_template('homepage.html', result=content)
+    result = get_shuffle_response(request=content, date=date.today(),
+                                  enable_extraction=True)
+    return render_template('homepage.html', result=result)
   return render_template('homepage.html')

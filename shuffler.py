2244f2e615964050a17a9f2534e9fd4ba2f0b46d
Heron Yang <heron.yang.tw@gmail.com>
2024-02-11 11:27:49 -0800

    Remove index, support empty name, and fixed randomize.

--- shuffler.py
+++ shuffler.py
@@ -3,35 +3,47 @@ from datetime import date, datetime
 
 from utils import extract_name_from_raw
 
+
 def extract_request_content(request):
   request = request[request.find('1.'):]
   request = request.split('- Venmo')[0]
   return request.strip()
 
-def get_shuffle_response(request, date, enable_extraction=False,
-                         enable_pair_on_duty=True):
+
+def get_random_seed(date):
+  return int(date.strftime('%Y%m%d'))
+
+
+def get_shuffle_response(request,
+                         date,
+                         enable_extraction=False,
+                         enable_pair_on_duty=True,
+                         enable_index_to_name=False):
   if enable_extraction:
     request = extract_request_content(request)
   names = extract_name_from_raw(request)
   slots = ['1:30', '2:00', '2:30', '3:00', '3:30', '4:00', '4:30', '5:00']
 
-  random.seed(str(date))
+  random.seed(get_random_seed(date))
   selected_num_multipier = 2 if enable_pair_on_duty else 1
   selected = random.sample(names, len(slots) * selected_num_multipier)
 
-  response = 'On-duty ' + str(date) + '\n--\n'
+  response = 'On-duty ' + str(date) + ' (' + str(
+      len(names)) + ' attended)\n--\n'
   for i in range(len(slots)):
-    end_slot = slots[i+1] if i + 1 < len(slots) else 'end'
+    end_slot = slots[i + 1] if i + 1 < len(slots) else 'end'
     if enable_pair_on_duty:
-      on_duty_name = get_printed_name(selected[i*2]) + ' + ' \
-          + get_printed_name(selected[i*2 + 1])
+      on_duty_name = get_printed_name(selected[i*2], enable_index_to_name) + \
+          ' + ' + get_printed_name(selected[i*2 + 1], enable_index_to_name)
     else:
-      on_duty_name = get_printed_name(selected[i])
+      on_duty_name = get_printed_name(selected[i], enable_index_to_name)
     response += slots[i] + '-' + end_slot + ': ' + on_duty_name + '\n'
   return response + get_instructions()
 
-def get_printed_name(name):
-  return name[0] + ' (' + name[1] + ')'
+
+def get_printed_name(name, enable_index_to_name):
+  return name[0] + ' (' + name[1] + ')' if enable_index_to_name else name[0]
+
 
 def get_instructions():
   return '''--
@@ -42,4 +54,3 @@ Instruction
 4. 結束時提醒下一個On-duty。
 --
 Note: On-Duty需要調整的人，自行跟其他團員協調即可。'''
-
--- utils.py
+++ utils.py
@@ -2,5 +2,11 @@ def extract_name(line):
   chunks = line.split('.')
   return chunks[1].strip(), chunks[0].strip()
 
+
 def extract_name_from_raw(raw):
-  return [extract_name(line) for line in raw.split('\n')[:-1]]
+  names = []
+  for line in raw.split('\n')[:-1]:
+    name = extract_name(line)
+    if name[0].strip():
+      names.append(name)
+  return names
